5ff85b46320aa760099a685a8f7ede807988c505
liuy1994 <liuy1994@outlook.com>
2024-05-21 17:45:44 +0800

    sh

--- container.sh
+++ container.sh
@@ -3,5 +3,5 @@
 if docker ps -a --filter="name=vue-build3" --format='{{.ID}}' ; then
   docker rm -f "vue-build3"
 else
-  echo "Container vue-build3 does not exist"
+  echo "Container vue-build3 does not exist !"
 fi
