f108e83541fde400348c5278b6e1daeaa8076005
liuy1994 <liuy1994@outlook.com>
2024-05-21 17:34:05 +0800

    sh

--- container.sh
+++ container.sh
@@ -2,7 +2,7 @@
 
 CONTAINER=vue-build3
 
-if docker ps -a --filter="id=$CONTAINER" --format='{{.ID}}' | grep -q "$CONTAINER"; then
+if docker ps -a --filter="name=$CONTAINER" --format='{{.ID}}' | grep -q "$CONTAINER"; then
   docker rm -f "$CONTAINER"
 else
   echo "Container $CONTAINER does not exist"
